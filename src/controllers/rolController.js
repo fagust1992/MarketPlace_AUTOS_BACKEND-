94c6b7b62063a0bd59f9ddb4bf50622918903092
francisco august <fraciscoaugust19921992@gmail.com>
2023-04-01 20:26:29 -0600

    agregando funcion crear rol

--- src/controllers/rolController.js
+++ src/controllers/rolController.js
@@ -2,15 +2,29 @@ const { Pool } = require("pg"); // traigo el pool
 const { credenciales } = require("../db/credentials");
 const pool = new Pool(credenciales); // instancio el pool
 
+const CreateRol = async (rol, roledescription) => {
+  if ((rol != "") & (roledescription != "")) {
+    try {
+      const consulta = "INSERT INTO rol VALUES (DEFAULT,$1, $2)";
+      const values = [rol, roledescription];
+      const result = await pool.query(consulta, values);
+      console.log(result);
+    } catch (error) {
+      throw new Error("ha fallado la consulta", { cause: error });
+    }
+  }
+};
 const obtenerRol = async () => {
   try {
     const { rows: roles } = await pool.query("SELECT * FROM rol");
+
     if (roles.length > 0) {
       return roles;
     }
   } catch (error) {
+    console.error("Error al intentar traer rolres :", error);
     throw new Error("ha fallado la conexion con los datos", { cause: error });
   }
 };
 
-module.exports = { obtenerRol };
+module.exports = { obtenerRol, CreateRol };
--- src/routes/Rol.js
+++ src/routes/Rol.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Importar funciones del controlador de roles
-const { obtenerRol } = require("../controllers/rolController");
+const { obtenerRol, CreateRol } = require("../controllers/rolController");
 
 router.get("/", async (request, response) => {
   try {
@@ -11,6 +11,18 @@ router.get("/", async (request, response) => {
   } catch (error) {
     response.status(500).json({ message: "Error al intentar traer roles" });
     throw new Error("ha fallado la conexion ", { cause: error });
+  }
+});
+
+router.post("/add_rol", async (request, response) => {
+  const { rol, roledescription } = request.body;
+
+  try {
+    await CreateRol(rol, roledescription);
+    response.send("rol agregado");
+  } catch (error) {
+    console.error("Error al intentar traer productos :", error);
+    response.status(500).json({ message: "Error al intentar agregar rol" });
   }
 });
 
